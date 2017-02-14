638e6ccc73f65bbcab47ef553bec9490a45e3a29
Harshad Devale <harshad.devale@tudip.nl>
2017-02-14 19:19:42 +0530

    --Added Zinnia blog to existing app.

--- revolv/urls.py
+++ revolv/urls.py
@@ -53,6 +53,22 @@ urlpatterns = patterns(
     url(r'social/', include('social.apps.django_app.urls', namespace='social')),
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's serving mechanism
+url(r'^blog/', include('zinnia.urls', namespace='zinnia')),
+url(r'^comments/', include('django_comments.urls')),
+url(r'^', include('zinnia.urls.capabilities')),
+    url(r'^search/', include('zinnia.urls.search')),
+    url(r'^sitemap/', include('zinnia.urls.sitemap')),
+    url(r'^trackback/', include('zinnia.urls.trackback')),
+    url(r'^blog/tags/', include('zinnia.urls.tags')),
+    url(r'^blog/feeds/', include('zinnia.urls.feeds')),
+    url(r'^blog/random/', include('zinnia.urls.random')),
+    url(r'^blog/authors/', include('zinnia.urls.authors')),
+    url(r'^blog/categories/', include('zinnia.urls.categories')),
+    url(r'^blog/comments/', include('zinnia.urls.comments')),
+    url(r'^blog/', include('zinnia.urls.entries')),
+    url(r'^blog/', include('zinnia.urls.archives')),
+    url(r'^blog/', include('zinnia.urls.shortlink')),
+    url(r'^blog/', include('zinnia.urls.quick_entry')),
 
     url(r'', include(wagtail_urls)),
 
