4eb643a1b921454c1f3b3f0230b2f3d3c80dc419
Harshad Devale <harshad.devale@tudip.nl>
2017-05-15 23:14:21 +0530

    Update: Bug #2752 Ability for re-volv staff to set up a matching donor
    Details:
    - Issue #2752
    - Fixed add matching donor related issue.

--- revolv/base/views.py
+++ revolv/base/views.py
@@ -100,6 +100,8 @@ class DonationReportView(UserDataMixin, TemplateView):
     template_name = 'base/partials/donation_report.html'
 
     def dispatch(self, request, *args, **kwargs):
+        if request.user.is_anonymous():
+            return HttpResponseRedirect(reverse("login"))
         if not request.user.revolvuserprofile.is_administrator():
             return HttpResponseRedirect(reverse("dashboard"))
         return super(DonationReportView, self).dispatch(request, *args, **kwargs)
@@ -125,6 +127,8 @@ class MatchingDonorsView(UserDataMixin, TemplateView):
     template_name = 'base/partials/matching_donors.html'
 
     def dispatch(self, request, *args, **kwargs):
+        if request.user.is_anonymous():
+            return HttpResponseRedirect(reverse("login"))
         if not request.user.revolvuserprofile.is_administrator():
             return HttpResponseRedirect(reverse("dashboard"))
         return super(MatchingDonorsView, self).dispatch(request, *args, **kwargs)
