7b2da322da62243d4b584e46ff4cf7a67dbb6fc1
Harshad Devale <harshad.devale@tudip.nl>
2017-06-14 15:57:19 +0530

    Update: Rework #3266 Generate financial report from the admin side
    Details:
    - Issue #3266
    - Added download Excel financial report from the admin side.

--- revolv/payments/admin.py
+++ revolv/payments/admin.py
@@ -82,11 +82,96 @@ def export_csv(modeladmin, request, queryset):
 export_csv.short_description = u"Export CSV"
 
 
+def export_xlsx(modeladmin, request, queryset):
+    import openpyxl
+    try:
+        from openpyxl.cell import get_column_letter
+    except ImportError:
+        from openpyxl.utils import get_column_letter
+
+    response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+    response['Content-Disposition'] = 'attachment; filename=RE-volv.xlsx'
+    wb = openpyxl.Workbook()
+    ws = wb.get_active_sheet()
+    ws.title = "RE-volv"
+
+    row_num = 0
+
+    columns = [
+        (u"FIRST NAME",30),
+        (u"LAST NAME",30),
+        (u"EMAIL",30),
+        (u"DATE",30),
+        (u"NAME OF PROJECT",30),
+        (u"DONATION TO SOLAR SEED FUND",30),
+        (u"REINVESTMENT IN SOLAR SEED FUND",20),
+        (u"ADMIN REINVESTMENT IN SOLAR SEED FUND",20),
+        (u"DONATION TO OPERATION",20),
+        (u"TOTAL DONATIONS",20),
+    ]
+
+    for col_num in xrange(len(columns)):
+        c = ws.cell(row=row_num + 1, column=col_num + 1)
+        c.value = columns[col_num][0]
+        ws.column_dimensions[get_column_letter(col_num + 1)].width = columns[col_num][1]
+
+    for payment in queryset:
+        if payment.admin_reinvestment:
+            admin_reinvestment = round(payment.amount, 2)
+        else:
+            admin_reinvestment = 0
+
+        if payment.user_reinvestment:
+            user_reinvestment = round(payment.user_reinvestment.amount, 2)
+        else:
+            user_reinvestment = 0
+
+        if payment.admin_reinvestment or payment.user_reinvestment:
+            donation_amount = 0
+        else:
+            donation_amount = payment.amount
+
+        if payment.tip:
+            tip = round(payment.tip.amount, 2)
+        else:
+            tip = 0
+
+        if payment.tip and payment.amount:
+            total = round(payment.tip.amount + payment.amount, 2)
+        if payment.tip and not payment.amount:
+            total = round(payment.tip.amount, 2)
+        if payment.amount and not payment.tip:
+            total = round(payment.amount, 2)
+        if not payment.amount and not payment.tip:
+            total = 0
+
+        row_num += 1
+        row = [
+            payment.user.user.first_name,
+            payment.user.user.last_name,
+            payment.user.user.email,
+            payment.created_at,
+            payment.project.title,
+            donation_amount,
+            user_reinvestment,
+            admin_reinvestment,
+            tip,
+            total,
+        ]
+        for col_num in xrange(len(row)):
+            c = ws.cell(row=row_num + 1, column=col_num + 1)
+            c.value = row[col_num]
+
+    wb.save(response)
+    return response
+
+export_xlsx.short_description = u"Export Excel"
+
 class Paymentadmin(admin.ModelAdmin):
 
     search_fields = ('user__user__username','user__user__first_name','user__user__last_name','user__user__email','amount')
 
-    actions = [export_csv, ]
+    actions = [export_csv, export_xlsx]
 
 admin.site.unregister(Payment)
 admin.site.register(Payment, Paymentadmin)
