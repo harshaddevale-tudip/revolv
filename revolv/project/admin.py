6c8b26ab24fd0702c85db20899baa29f2122895b
Harshad Devale <harshad.devale@tudip.nl>
2017-07-17 21:10:24 +0530

    -- Added StripeDetails table in admin portal.

--- revolv/project/admin.py
+++ revolv/project/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Category, DonationLevel, Project, ProjectUpdate, ProjectMatchingDonors
+from .models import Category, DonationLevel, Project, ProjectUpdate, ProjectMatchingDonors,StripeDetails
 
 admin.site.register(Category)
 admin.site.register(DonationLevel)
 admin.site.register(Project)
 admin.site.register(ProjectUpdate)
 admin.site.register(ProjectMatchingDonors)
+admin.site.register(StripeDetails)
